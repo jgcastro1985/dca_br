4b1867f919403c57cfed459f3af2fbb2a55ed4d0
Bruno C <bruno.cavalcante2121@outlook.com.br>
2021-05-23 02:03:57 -0300

    Primeira versão da API

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from flask import Flask, request, redirect, url_for
 from flask import render_template
+import pandas as pd
+import sqlite3
+
+conn = sqlite3.connect(r"bd/database.db")
+df = pd.read_sql("select * from dados_diarios_btc_brl", conn)
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/dados')
+def dados_btc():
+    return df.to_json(orient='records')
 
 
 app.run(debug=True)
